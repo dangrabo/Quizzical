514e78e95c8e5f261a35cdda17711e96f0c17e32
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-27 19:11:32 -0700

    finished most of it

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
 import { useState, useEffect } from "react";
+import he from 'he';
 import Question from "./Question";
 
 function App() {
   const [showHome, setShowHome] = useState(true);
   const [questions, setQuestions] = useState(null);
+  const [showAnswers, setShowAnswers] = useState(false);
+  const [selected, setSelected] = useState([]);
 
   // fetch data
   useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
-      .then(res => res.json())
-      .then(data => {
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((res) => res.json())
+      .then((data) => {
         if (data.response_code == 0) setQuestions(data.results);
-      })
+      });
   }, []);
 
   // generate question components in array
   let questionsCompArray;
   if (questions != null) {
-    questionsCompArray = questions.map(question => (<Question key={question.correct_answer} questionObj={question} />))
+    questionsCompArray = questions.map((question, index) => (
+      <Question
+        key={index}
+        questionObj={question}
+        id={index}
+        showAnswers={showAnswers}
+        selected={selected[index]}
+      />
+    ));
   }
 
   // check answers
-  function checkAnswers(answers) {
-    console.log(answers);
+  function checkAnswers(formData) {
+    const selectedAnswers = [];
+    for (let i = 0; i < 5; i++) {
+      selectedAnswers.push(formData.get(`${i}`));
+    }
+
+    if (selectedAnswers.includes(null)) {
+      alert('Please select an answer for each question');
+      return
+    } 
+
+    getNumRight();
+    setSelected(selectedAnswers);
+    setShowAnswers(true);
+  }
+
+  function getNumRight() {
+    let numRight = 0;
+    
   }
 
   // show home screen
@@ -42,10 +70,28 @@ function App() {
     <div id="questions-div" className="main-container">
       <form action={checkAnswers}>
         {questionsCompArray}
-        <button>Check answers</button>
+        {!showAnswers ? (
+          <button className="check-again-btn">Check answers</button>
+        ) : (
+          <div id="play-again-div">
+            <p>You scored 3/5 correct answers</p>
+            <button type="button" className="check-again-btn">Play again</button>
+          </div>
+        )}
       </form>
     </div>
-  )
+  );
 }
 
 export default App;
+
+
+/* TODO:
+
+home screen styling
+Fix the styling on the bottom div after submitting answers
+fix fucntionality for play again button
+adjust functionality to check that all answers were selected before submitting form
+
+
+  */
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,25 +1,36 @@
-import he from 'he';
+import he from "he";
 
-export default function Question({ questionObj }) {
-    const { correct_answer, incorrect_answers  } = questionObj;
+export default function Question({ questionObj, id, showAnswers, selected }) {
+  const { correct_answer, incorrect_answers } = questionObj;
 
-    const question = he.decode(questionObj.question)
-    const correctIndex = Math.floor(Math.random() * 4);
-    const allAnswers = incorrect_answers.toSpliced(correctIndex, 0, correct_answer).map(answer => he.decode(answer));
+  const question = he.decode(questionObj.question);
+  const correctAnswer = he.decode(correct_answer);
+  const correctIndex = Math.floor(Math.random() * 4);
+  const allAnswers = incorrect_answers
+    .toSpliced(correctIndex, 0, correct_answer)
+    .map((answer) => he.decode(answer));
 
-    const allAnswersRadios = allAnswers.map(answer => (
-        <label>
-            {answer}
-            <input type="radio" name={correct_answer} value={answer} />
-        </label>
-    ));
-
+  const allAnswersRadios = allAnswers.map((answer, index) => {
+    let answerClass = 
+        answer === correctAnswer ? 'correct' :
+        selected === answer ? 'incorrect' :
+        'opaque';
+    
     return (
-        <>
-        <h3>{question}</h3>
-        <div>{allAnswersRadios}</div>
-        <hr />
-        </>
-    )
+      <div key={index}>
+        <input type="radio" id={`${id}${index}`} name={id} value={answer} />
+        <label className={showAnswers ? answerClass : ''} htmlFor={`${id}${index}`}>
+          {answer}
+        </label>
+      </div>
+    );
+  });
 
+  return (
+    <>
+      <h3>{question}</h3>
+      <div className="answers">{allAnswersRadios}</div>
+      <hr />
+    </>
+  );
 }
